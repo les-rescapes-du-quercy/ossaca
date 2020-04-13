c2f92e75708d2bc651035be7ceae55c96c845466
Maxime Chevallier <chevallier.seb.max@gmail.com>
2020-04-13 09:40:44 +0200

    plugin/garradin: Add default values for config values
    Signed-off-by: Maxime Chevallier <chevallier.seb.max@gmail.com>

--- plugins/plugin_garradin.py
+++ plugins/plugin_garradin.py
@@ -9,10 +9,22 @@ class GarradinPlugin(OssacaPersonProviderPlugin):
     '''
     Plugin to retrieve Person infos in a garradin database
     '''
+
+    default_config = {
+            "database_path" : "/var/www/garradin/association.sqlite"
+    }
+
     def __init__(self):
-        OssacaPersonProviderPlugin.__init__(self, "test_person_plugin2")
+        OssacaPersonProviderPlugin.__init__(self, "garradin_plugin")
+
+    def __set_default_config(self):
+        for key, val in GarradinPlugin.default_config.items():
+            if self.get_config(key) is None:
+                self.set_config(key, val)
 
     def load(self):
+        self.__set_default_config()
+
         return True
 
     def destroy(self):
